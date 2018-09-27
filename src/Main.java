42146076b415d58ba0e2a75c3af77ad450342159
Jasraj Singh Bedi <j_ed@encs.concordia.ca>
2018-09-26 20:58:49 -0400

    added map directory in the assets directory;made the code more modular

--- src/Main.java
+++ src/Main.java
@@ -15,13 +15,69 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
 public class Main{
-	 public static void print(String str) 
-	 {
-		 System.out.print(str);
-	 }
+	
      public static void main(String []args) throws IOException
      {
-    	 Map m = new Map();
+    	Map m = new Map();
+    	m.createMap();
+    	m.returnArrayListOfMapNames();
+     }
+}
+
+class Map
+{
+	HashMap<String,Integer> controlValuesByContinents = new HashMap<String,Integer>();
+	HashMap<String,ArrayList<String>> territories = new HashMap<String,ArrayList<String>>();
+	public void addControlValues(String country,int controlValue)
+	{
+		controlValuesByContinents.put(country,controlValue);
+	}
+	public void addTeritorries(String teritorry,ArrayList<String> list)
+	{
+		territories.put(teritorry,list);
+	}
+	public void saveMap(String mapName) throws IOException
+	{
+		String content = "[Map]\r\n \r\n[Continents]";
+		for (String key : controlValuesByContinents.keySet()) 
+		{
+			content = content +"\r\n"+key+"="+controlValuesByContinents.get(key);
+		}
+		content = content +"\r\n";
+		content = content +"\r\n";
+		content = content +"[Territories]\r\n";
+		for (String key : territories.keySet())
+		{
+			String teritorryVal = "";
+			content = content +key+",";
+			for (int k = 0; k < territories.get(key).size(); k++) 
+			{
+				teritorryVal = teritorryVal + territories.get(key).get(k);
+				if(k==territories.get(key).size() - 1) {
+					
+				}else {
+					teritorryVal = teritorryVal +",";
+				}
+			}
+			content = content + teritorryVal+"\r\n";
+		}
+	    final Path path = Paths.get("assets/maps/"+mapName+".map");
+
+	    try (
+	        final BufferedWriter writer = Files.newBufferedWriter(path,
+	            StandardCharsets.UTF_8, StandardOpenOption.CREATE);
+	    ) {
+	        writer.write(content);
+	        writer.flush();
+	    }
+	}
+	public static void print(String str) 
+	{
+		System.out.print(str);
+	}
+	
+	public void createMap() {
+		 Map m = new Map();
     	 GetInputs inp = new GetInputs();
     	 print("\nEnter the number of continents you want to create\n");
     	 int totalNumberOfCountries = inp.getNextInteger();
@@ -54,56 +110,45 @@ public class Main{
         	 }
         	 m.territories.put(territory, territoryValues);
     	 }
-    	 m.saveMap();
-     }
-}
-
-class Map
-{
-	HashMap<String,Integer> controlValuesByContinents = new HashMap<String,Integer>();
-	HashMap<String,ArrayList<String>> territories = new HashMap<String,ArrayList<String>>();
-	public void addControlValues(String country,int controlValue)
-	{
-		controlValuesByContinents.put(country,controlValue);
-	}
-	public void addTeritorries(String teritorry,ArrayList<String> list)
-	{
-		territories.put(teritorry,list);
-	}
-	public void saveMap() throws IOException
-	{
-		String content = "[Map]\r\n \r\n[Continents]";
-		for (String key : controlValuesByContinents.keySet()) 
-		{
-			content = content +"\r\n"+key+"="+controlValuesByContinents.get(key);
+    	 print("\nEnter the name of the map ");
+    	 String mapName = inp.getNextString();
+    	 try {
+			m.saveMap(mapName);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		content = content +"\r\n";
-		content = content +"\r\n";
-		content = content +"[Territories]\r\n";
-		for (String key : territories.keySet())
-		{
-			String teritorryVal = "";
-			content = content +key+",";
-			for (int k = 0; k < territories.get(key).size(); k++) 
-			{
-				teritorryVal = teritorryVal + territories.get(key).get(k);
-				if(k==territories.get(key).size() - 1) {
-					
-				}else {
-					teritorryVal = teritorryVal +",";
-				}
+	}
+	public void parseMap(String mapName) {
+		
+	}
+	public void editMap() {
+		
+	}
+	
+
+	/**
+	 * Returns an array list of all the maps in the Maps folder directory.
+	 * <p>
+	 * Returns null if there are no maps in the map folder.
+
+	 * @return      an arraylist of all the maps in the map folder
+	 * @see         list
+	 */
+	public ArrayList<String> returnArrayListOfMapNames() {
+		ArrayList<String> fileNames = new ArrayList<String>();
+		File folder = new File("assets/maps");
+		File[] listOfFiles = folder.listFiles();
+
+		for (int i = 0; i < listOfFiles.length; i++) {
+			if (listOfFiles[i].isFile()) {
+				System.out.println("File " + listOfFiles[i].getName());
+				fileNames.add(listOfFiles[i].getName());
+			} else if (listOfFiles[i].isDirectory()) {
+				System.out.println("Directory " + listOfFiles[i].getName());
 			}
-			content = content + teritorryVal+"\r\n";
 		}
-	    final Path path = Paths.get("file.map");
-
-	    try (
-	        final BufferedWriter writer = Files.newBufferedWriter(path,
-	            StandardCharsets.UTF_8, StandardOpenOption.CREATE);
-	    ) {
-	        writer.write(content);
-	        writer.flush();
-	    }
+		return fileNames;
 	}
 }
 
@@ -118,3 +163,4 @@ class GetInputs{
     	return sc.nextInt();
     }
 }
+
