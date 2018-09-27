9047c6061cb8f0db9012c83639a3777cbe75fa87
binaykumar19 <binaykumar19@gmail.com>
2018-09-27 10:22:06 -0400

    created GameController.java

---
+++ src/com/risk/controller/GameController.java
@@ -0,0 +1,12 @@
+package com.risk.controller;
+
+/**
+ * @author Binay Kumar
+ * @version 1.0.0
+ * @since 27-September-2018
+ * This class is used to handle Game Operations.
+ */
+
+public class GameController {
+
+}
