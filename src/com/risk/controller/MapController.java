8bf07fc65dbe084483e0731cc2c41dfbfe9e6e6c
binaykumar19 <binaykumar19@gmail.com>
2018-09-27 10:16:22 -0400

    updated MapController.java

--- src/com/risk/controller/MapController.java
+++ src/com/risk/controller/MapController.java
@@ -4,8 +4,19 @@ package com.risk.controller;
  * @author Binay Kumar
  * @version 1.0.0
  * @since 27-September-2018
+ * This class is used to handle operations related to MAP.
  */
 
 public class MapController {
 
+	private void createMap()
+	{
+		System.out.println("Create Map on MapController called");
+	}
+	
+	private void editMap()
+	{
+		System.out.println("Edit Map on MapController called");
+	}
+	
 }
