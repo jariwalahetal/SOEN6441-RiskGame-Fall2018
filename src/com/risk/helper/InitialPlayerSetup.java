2620c5a6bd5a63564f878292dd5ffae81166ba8b
binaykumar19 <binaykumar19@gmail.com>
2018-09-30 19:11:04 -0400

    added Color and Initial Army logic

---
+++ src/com/risk/helper/InitialPlayerSetup.java
@@ -0,0 +1,36 @@
+package com.risk.helper;
+
+public class InitialPlayerSetup {
+
+	public static Color getPlayerColor(int playerID) {
+		switch (playerID) {
+		case 1:
+			return Color.BLACK;
+		case 2:
+			return Color.BLUE;
+		case 3:
+			return Color.GREEN;
+		case 4:
+			return Color.RED;
+		case 5:
+			return Color.YELLOW;
+		default:
+			return Color.YELLOW;
+		}
+	}
+
+	public static int getInitialArmyCount(int playerCount) {
+		switch (playerCount) {
+		case 2:
+			return 50;
+		case 3:
+			return 35;
+		case 4:
+			return 30;
+		case 5:
+			return 25;
+		default:
+			return 20;
+		}
+	}
+}
