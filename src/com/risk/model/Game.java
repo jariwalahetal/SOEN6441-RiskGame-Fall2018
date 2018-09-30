fe75c6a9a77350a7db4be0bc852c2b962eff15a9
binaykumar19 <binaykumar19@gmail.com>
2018-09-30 14:52:43 -0400

    added Game.java

---
+++ src/com/risk/model/Game.java
@@ -0,0 +1,63 @@
+package com.risk.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Game Class
+ * 
+ * @author Binay
+ * @version 1.0.0
+ * @since 30-September-2018
+ */
+public class Game {
+
+	// Country and the army assigned to it
+	private HashMap<Country, Integer> countryArmy = new HashMap<Country, Integer>();
+	private ArrayList<Player> playerList = new ArrayList<Player>();
+	private Map map;
+
+	/**
+	 * This is a constructor of Game class which will initialise the Map
+	 * 
+	 * @param map
+	 */
+	public Game(Map map) {
+		super();
+		this.map = map;
+	}
+
+	/**
+	 * This function will randomly assign Countries to all players
+	 * 
+	 * @param map
+	 */
+	public void assignCountryToPlayer() { // divide the countries in Map object to the players in the playerList
+		System.out.println("assignCountryToPlayer called");
+	}
+
+	/**
+	 * This function will assign armies to all players
+	 */
+	public void initialArmyAssignment() {// set NoOfArmies field for all players.
+		System.out.println("initialArmyAssignment called");
+	}
+
+	/**
+	 * This function will add the player to the game(playerList)
+	 * 
+	 * @param player
+	 */
+	public void addPlayer(Player player) {
+		this.playerList.add(player);
+	}
+
+	/**
+	 * This function will tell if we have found a winner or not.
+	 * 
+	 * @return
+	 */
+	public Boolean isGameOn() {
+		return false;
+	}
+}
