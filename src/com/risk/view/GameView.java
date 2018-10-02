081f92cb2138f6d737d318900174dd04885e3397
sadgi <sadgi.mahajan@yahoo.com>
2018-10-01 20:16:26 -0400

    MAP REINFORCEMENT VIEW
    MAP REINFORCEMENT VIEW

--- src/com/risk/view/GameView.java
+++ src/com/risk/view/GameView.java
@@ -1,10 +1,13 @@
 package com.risk.view;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -15,6 +18,10 @@ import javax.swing.JScrollPane;
 import javax.swing.border.TitledBorder;
 
 import com.risk.model.Game;
+
+
+
+
 
 
 
@@ -60,7 +67,8 @@ public class GameView {
 	  public void gameInitializer(){
 			 gameJframe = new JFrame("Risk Game");
 			    loadGameActionView();
-			    gameJframe.add(gameActionJpanel);				  
+			    gameJframe.add(gameActionJpanel);	
+			    loadingReinforcementLabel();
 			    gameJframe.setSize(1200, 700);
 			    gameJframe.setVisible(true);
 			    gameJframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -79,12 +87,65 @@ public class GameView {
 		    } catch (IOException e) {
 		      e.printStackTrace();
 		    }
+		 /*
+		  *NEED THIS INFO FROM CONTROLLER
+		  *    for (int i = 0; i < Map.getCountryList().size(); i++) {
+		        Country tempCountry = Map.getCountryList().get(i);
+		        int[] coordinate = tempCountry.getCoordinate();
+		        tempCountry.setPointInMapLabel(new JLabel("" + tempCountry.getSoilders()));
+		        tempCountry.getPointInMapLabel().setFont(new Font("Courier", Font.BOLD, 20));
+		        tempCountry.getPointInMapLabel().setForeground(tempCountry.getPlayer().getColor());
+		        tempCountry.getPointInMapLabel().setBounds(coordinate[0], coordinate[1], 25, 25);
+		        mapLabel.add(tempCountry.getPointInMapLabel());
+		      } */
 		    mapLabel = new JLabel(icon);
 		    mapScrollPane = new JScrollPane(mapLabel);
 		    mapScrollPane.setBounds(10, 10, 500, 500);
 		    mapScrollPane.setBorder(new TitledBorder("Map"));
 		    gameActionJpanel.add(mapScrollPane);
 		    
+	  }
+	  
+	  private  void loadingReinforcementLabel(){
+		    reinforcementLabel.removeAll();
+		    reinforcementLabel = null;
+		    reinforcementLabel = new JLabel();
+		    reinforcementLabel.setBorder(BorderFactory.createTitledBorder(null, "ReinforcementPhase",
+		        TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION,
+		        new Font("SansSerif", Font.PLAIN, 12), Color.RED));
+		    reinforcementLabel.setBounds(mapScrollPane.getX()+500,
+		        mapScrollPane.getY(), mapScrollPane.getWidth()+200,
+		     200);
+		 // Recreate every components in Label
+		    playersTurnLabel = new JLabel(
+		        "Player A");
+		    playersTurnLabel.setBorder(new TitledBorder("Player's Turn"));
+		    playersTurnLabel.setBounds(5, 20, reinforcementLabel.getWidth() / 2 - 8,
+		        reinforcementLabel.getHeight() / 3 - 15);
+		    
+		    // listOfCountriesPlayerOccupied(Map.playerWithTurn());
+		  //  String[] countryNameList = Map.playerWithTurn().listOfCountriesPlayerConqueredInStringArray();
+		    String[] countryNameList={"INDIA","PAKISTAN","NEPAL"};
+		    addUnitToCountryComboBox = new JComboBox<>(countryNameList);
+		    addUnitToCountryComboBox.setBorder(new TitledBorder("Add Unit To Country"));
+		    addUnitToCountryComboBox.setBounds(playersTurnLabel.getX() + playersTurnLabel.getWidth() + 3,
+		        playersTurnLabel.getY(), playersTurnLabel.getWidth(), playersTurnLabel.getHeight());
+		    
+		    unassignedUnit = new JLabel("UNASSIGNED VALUE=10");
+		    unassignedUnit.setBorder(new TitledBorder("Unassigned Unit"));
+		    unassignedUnit.setBounds(playersTurnLabel.getX(),
+		    		playersTurnLabel.getY() + playersTurnLabel.getHeight() + 5, playersTurnLabel.getWidth(),
+		        playersTurnLabel.getHeight());
+		    addUnitButton.setBounds(addUnitToCountryComboBox.getX(), unassignedUnit.getY(),
+		            unassignedUnit.getWidth(), unassignedUnit.getHeight());
+
+		    reinforcementLabel.add(playersTurnLabel);
+		    reinforcementLabel.add(playersTurnLabel);
+		    reinforcementLabel.add(unassignedUnit);
+		    reinforcementLabel.add(addUnitToCountryComboBox);
+		    reinforcementLabel.add(addUnitButton);
+		    gameActionJpanel.add(reinforcementLabel);
+		  
 	  }
 	  
 	  public static void main(String[] args) {
