1609e6137015695b26942b103390e65fbe64b13e
Jasraj Singh Bedi <jasrajqwe@gmail.com>
2018-10-14 20:50:48 -0400

    Added country test class

---
+++ test/com/risk/model/CountryTest.java
@@ -0,0 +1,36 @@
+package com.risk.model;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.*;
+
+/**
+ *This class will test the country methods.
+ */
+public class CountryTest {
+	Country c1;
+	String neighbourToadd;
+	ArrayList<String> neighbourStringList;
+
+	/**
+	 *This method will setup the variables for the test methods.
+	 */
+	@Before
+	public void setUp() {
+		c1 = new Country(12, "TheBestCountry", 2);
+		neighbourToadd = "neighbourCountry";
+	}
+	
+	/**
+	 *This method tests if the addneighbour method actually adds the country into the list or not.
+	 */
+	@Test
+	public void checkAddNeighbour() {
+		c1.addNeighboursString(neighbourToadd);
+		neighbourStringList = c1.getNeighboursString();
+		assertTrue(neighbourStringList.contains(neighbourToadd));
+	}
+
+}
