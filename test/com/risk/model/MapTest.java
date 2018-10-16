4fba8047ac25b823eece40bf574ea6e60047807a
Jasraj Singh Bedi <jasrajqwe@gmail.com>
2018-10-16 00:17:48 -0400

    readmap test case for countries

--- test/com/risk/model/MapTest.java
+++ test/com/risk/model/MapTest.java
@@ -17,6 +17,7 @@ public class MapTest {
 	StringBuffer sb2;
 	Map map1;
 	ArrayList<String> testMapContinents = new ArrayList<String>();
+	ArrayList<String> testMapCountries = new ArrayList<String>();
 	
 	public  boolean equalLists(ArrayList<String> one, ArrayList<String> two){     
 	    if (one == null && two == null){
@@ -54,6 +55,48 @@ public class MapTest {
 		testMapContinents.add("Central Africa");
 		testMapContinents.add("The Horn");
 		testMapContinents.add("The Congo");
+		testMapCountries.add("Morocco");
+		testMapCountries.add("Algeria");
+		testMapCountries.add("Tunisia");
+		testMapCountries.add("Libya");
+		testMapCountries.add("Egypt");
+		testMapCountries.add("Western Sahara");
+		testMapCountries.add("Mauritania");
+		testMapCountries.add("Senegal");
+		testMapCountries.add("Mali");
+		testMapCountries.add("Guinea");
+		testMapCountries.add("Sierra Leone");
+		testMapCountries.add("Liberia");
+		testMapCountries.add("Ivory Coast");
+		testMapCountries.add("Burkina Faso");
+		testMapCountries.add("Benin");
+		testMapCountries.add("Ghana");
+		testMapCountries.add("Niger");
+		testMapCountries.add("Nigeria");
+		testMapCountries.add("Chad");
+		testMapCountries.add("Cameroon");
+		testMapCountries.add("Central African Rep.");
+		testMapCountries.add("North Sudan");
+		testMapCountries.add("South Sudan");
+		testMapCountries.add("Eritrea");
+		testMapCountries.add("Ethiopia");
+		testMapCountries.add("Somalia");
+		testMapCountries.add("Gabon");
+		testMapCountries.add("Rep. of the Congo");
+		testMapCountries.add("D.R.O.T. Congo");
+		testMapCountries.add("Angola");
+		testMapCountries.add("Zambia");
+		testMapCountries.add("Uganda");
+		testMapCountries.add("Kenya");
+		testMapCountries.add("Tanzania");
+		testMapCountries.add("Malawi");
+		testMapCountries.add("Namibia");
+		testMapCountries.add("Botswana");
+		testMapCountries.add("Zimbabwe");
+		testMapCountries.add("Mozambique");
+		testMapCountries.add("Madagascar");
+		testMapCountries.add("South Africa");
+		
 		Collections.sort(testMapContinents);
 		String inValidMapString ="[Map]\n" + 
 				 "author=SOEN6441 - Team8\n" +  
@@ -110,12 +153,21 @@ public class MapTest {
 	@Test
 	public void readMapTest() {
 		ArrayList<String> testContinents= new ArrayList<String>();
+		ArrayList<String> testCountries= new ArrayList<String>();
 		map1.readMap();
 		ArrayList<Continent> a =map1.getContinentList();
 		for(Continent i: a) {
 			testContinents.add(i.getContName());
+			for(Country x:i.getCountryList()) {
+				if(testCountries.contains(x.getCountryName())) {
+					//nada
+				}else {
+					testCountries.add(x.getCountryName());
+				}
+			}
 		}
 		Collections.sort(testContinents);
 		assertTrue(equalLists(testContinents,testMapContinents));
+		assertTrue(equalLists(testCountries,testMapCountries));
 	}
 }
