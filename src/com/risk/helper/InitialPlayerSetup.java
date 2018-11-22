cb1f73bb59644e3e740520233dab797c97d93653
Hetal Jariwala <hetal.h.jariwala@gmail.com>
2018-11-21 20:49:31 -0500

    Changed InitialPlayerSetup for testing

--- src/com/risk/helper/InitialPlayerSetup.java
+++ src/com/risk/helper/InitialPlayerSetup.java
@@ -38,11 +38,11 @@ public class InitialPlayerSetup {
 	public static int getInitialArmyCount(int playerCount) {
 		switch (playerCount) {
 		case 3:
-			return 35;
+			return 20;
 		case 4:
-			return 30;
+			return 15;
 		case 5:
-			return 25;
+			return 15;
 		default:
 			return 20;
 		}
