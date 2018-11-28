50c1d4c5579c9e219dbb1f4edef847c2182be9e8
Hetal Jariwala <hetal.h.jariwala@gmail.com>
2018-11-28 00:19:17 -0500

    - Bug fixes for cheater PlayerStrategy

--- src/com/risk/model/strategies/Cheater.java
+++ src/com/risk/model/strategies/Cheater.java
@@ -3,6 +3,8 @@ package com.risk.model.strategies;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import com.risk.helper.Common;
 import com.risk.helper.IOHelper;
@@ -30,53 +32,78 @@ public class Cheater implements PlayerStrategy, Serializable {
 
 	@Override
 	public boolean reinforce(Player player) {
-		// TODO Auto-generated method stub
-	    for (Country country:player.getAssignedCountryList())
-	    { IOHelper.print("Adding reinforcement army in " + country.getCountryName());
-		  int armies =  country.getnoOfArmies();
-	      player.setNoOfReinforcedArmies(0);   //TODO: need to confim it 
-	      country.setNoOfArmies(armies*2);
+	    for (Country country:player.getAssignedCountryList()) { 
+	    	IOHelper.print("Adding reinforcement army in " + country.getCountryName() + "("+ country.getnoOfArmies()+")");
+	    	int armies =  country.getnoOfArmies();
+	    	player.setNoOfReinforcedArmies(0);
+	    	country.setNoOfArmies(armies*2);
+			IOHelper.print("Added reinforcement army in " + country.getCountryName() + "("+ country.getnoOfArmies()+")");
 	    }
 		return true;	
 }
 
 	@Override
 	public void attack(Player attackerPlayer) {
-		// TODO Auto-generated method stub		
 		int armies;
 		Player defenderPlayer;
 		
-		ArrayList<Country> attackerCountries = attackerPlayer.getAssignedCountryList();
-		int size = attackerCountries.size();
-		for (int i=0;i<size;i++) { 
-			Country fromCountry = attackerCountries.get(i);
-			for(Country toCountry : fromCountry.getNeighbourCountries()) {  
-				if(toCountry.getPlayer().getPlayerId() != attackerPlayer.getPlayerId()) {
-					armies = toCountry.getnoOfArmies();
-	 	            toCountry.decreaseArmyCount(armies);   	
-	                defenderPlayer = toCountry.getPlayer();
-	                
-	                attackerPlayer.setAttackedPlayer(defenderPlayer);
-	        		attackerPlayer.setFromCountry(fromCountry);
-	        		attackerPlayer.setToCountry(toCountry);
-	        		
-	                attackerPlayer.conquerCountry(defenderPlayer);
-				}
+		ArrayList<Integer> attackerCountryIds = attackerPlayer.getAssignedCountryList()
+											.stream().map(c -> c.getCountryId())
+											.collect(Collectors.toCollection(ArrayList::new));
+		IOHelper.print("Cheater player " + attackerPlayer.getName() + " found " + attackerCountryIds.size() +" countries for attack. Doing attack now..");
+		for (int i=0;i<attackerCountryIds.size();i++) { 
+			int attackerCountryId = attackerCountryIds.get(i);
+			Country fromCountry = attackerPlayer.getAssignedCountryList()
+									.stream().filter(x -> x.getCountryId() == attackerCountryId)
+									.findFirst().orElse(null);
+			
+			if(fromCountry == null) {
+				continue;
+			}
+			ArrayList<Country> CountriesToAttack = attackerPlayer.getUnAssignedNeighbouringCountriesObject
+					(fromCountry.getCountryName());
+			if(CountriesToAttack==null || CountriesToAttack.size() == 0)
+			{
+				continue;
+			}
+			for(Country toCountry : CountriesToAttack) {  
+				if(toCountry.getPlayer().getPlayerId() == attackerPlayer.getPlayerId())
+					continue;
+				
+				IOHelper.print(fromCountry.getCountryName() + " is attacking ("+ fromCountry.getnoOfArmies()+")" + toCountry.getCountryName() + "("+ toCountry.getnoOfArmies()+ ")");
+				armies = toCountry.getnoOfArmies();
+				toCountry.decreaseArmyCount(armies);   	
+	            defenderPlayer = toCountry.getPlayer();
+	            
+	            attackerPlayer.setAttackedPlayer(defenderPlayer);
+	            attackerPlayer.setFromCountry(fromCountry);
+	            attackerPlayer.setToCountry(toCountry);
+	        	
+	            attackerPlayer.conquerCountry(defenderPlayer);
+	            
+	            // reverting minimum army rule to overcome - armies count
+	    		fromCountry.increaseArmyCount(1);
+	    		toCountry.decreaseArmyCount(1);
 	    	}	    	
-	    }		
+	    }	
+		IOHelper.print("Finishing attack...");	
 	}
 
 	@Override
 	public boolean fortify(Player player) {
-		// TODO Auto-generated method stub
         int armiesCount;
-		for (Country country:player.getAssignedCountryList())
-	    { for(Country neighbourCountry : country.getNeighbourCountries())
-	    	{ if (neighbourCountry.getPlayer().getPlayerId()!=player.getPlayerId())
-	    	  { armiesCount = country.getnoOfArmies()*2;
-	    		country.increaseArmyCount(armiesCount);
-	    	  }
-	    	}		
+		for (Country country:player.getAssignedCountryList()) { 
+			IOHelper.print("Cheater player " + player.getName() +" is trying to fortify " + country.getCountryName() + "("+ country.getnoOfArmies() +")");
+			ArrayList<Country> neighbouringList = player.getUnAssignedNeighbouringCountriesObject(country.getCountryName());
+			
+			if(neighbouringList != null || neighbouringList.size() == 0) {
+				IOHelper.print("-- Cannot fortify as there is no neigbouring county found from other player");
+			}
+			else {
+				armiesCount = country.getnoOfArmies()*2;
+				country.increaseArmyCount(armiesCount);
+				IOHelper.print("-- Finished fortification with country " +  country.getCountryName() + " ("+ country.getnoOfArmies() +")");
+			}	
 	    }
 		return true;
 	}
